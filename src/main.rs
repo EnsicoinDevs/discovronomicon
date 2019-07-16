3dcab7474b2a7d401f569ba0d61e4b0e75a50f73
Quentin Boyer <qbsecond@gmail.com>
2019-07-16 02:52:34 +0200

    it started

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,35 @@
 #![feature(async_await)]
 
 use tokio;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+//use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::codec::Encoder;
+use tokio::codec::Framed;
 use tokio::net::TcpListener;
+use futures::{SinkExt, StreamExt};
+use bytes::BytesMut;
 
 use std::net::SocketAddr;
+
+mod discover_message;
+use discover_message::{Message,MessageCodec};
 
 #[tokio::main]
 async fn main() {
     let addr = SocketAddr::from(([0,0,0,0], 3333));
     let mut listener = TcpListener::bind(&addr).unwrap();
     println!("Listening on: {}", addr);
+    
+    let mut bytes = BytesMut::new();
+    MessageCodec::new().encode(Message::Ping, &mut bytes).unwrap();
+    println!("message: {:?}", bytes);
 
     loop {
-        let (mut socket, _) = listener.accept().await.unwrap();
+        let (socket, _) = listener.accept().await.unwrap();
 
         tokio::spawn(async move {
-            let mut buf = [0; 1024];
-            loop {
-                let n = socket.read(&mut buf).await.unwrap();
-
-                if n == 0 {
-                    return
-                }
-                socket.write_all(&buf[0..n]).await.unwrap();
+            let mut messages = Framed::new(socket, MessageCodec::new());
+            while let Some(message) = messages.next().await {
+                println!("Message: {:?}", message);
             }
         });
     }
